c3639aac6dd037aae4d78227b3a59d7a57d51a35
dongho <zzw123@naver.com>
2024-07-15 17:00:59 +0900

    ..

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1, num2, result;
+        String operator;
+
+        System.out.print("수식을 입력하시오 : ");
+        String expression = sc.nextLine();
+
+        String[] stChange = expression.split("\\s");
+        num1 = Integer.parseInt(stChange[0]);
+        num2 = Integer.parseInt(stChange[2]);
+        operator = stChange[1];
+
+        switch (operator){
+            case "+":
+                result = add(num1, num2);
+                break;
+            case "-":
+                result = sub(num1, num2);
+                break;
+            case "*":
+                result = mul(num1, num2);
+                break;
+            case "/":
+                result = div(num1, num2);
+                break;
+        }
+
+        System.out.println("결과는 : " + result + "입니다.");
+
+        int add(int a, int b){
+            return 0;
+        }
+
+        int sub(int a, int b){
+            return 0;
+        }
+
+        int mul(int a, int b){
+            return 0;
+        }
+
+        int div(int a, int b){
+            return 0;
+        }
+
+
+    }
+
+
+
+}
