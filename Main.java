ecca382d1b2ce6bda424b334eae8e20a28d308f1
junyong park <pjy37414@gmail.com>
2024-07-15 17:37:50 +0900

    pjy수정

--- Main.java
+++ Main.java
@@ -40,11 +40,11 @@ public class Main {
     }
 
     public static int add(int a, int b){
-        return 0;
+        return a + b;
     }
 
     public static int sub(int a, int b){
-        return 0;
+        return a - b;
     }
 
     public static int mul(int a, int b){
